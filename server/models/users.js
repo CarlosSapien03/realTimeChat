a33dcd72028366ec5d4361403fa09b43231c4340
CarlosSapien03 <cssc2003@gmail.com>
2025-05-22 01:15:33 -0700

    Modelado de users.js creado.

---
+++ server/models/users.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      "Por favor ingresa un correo válido",
+    ],
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
